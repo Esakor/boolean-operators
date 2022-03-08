cc5d0c8d0d927a43d1b58add2075d269ecced8a5
Esakor <100299697+Esakor@users.noreply.github.com>
2022-03-08 11:05:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    if (var1 > 0 && var2 > 0) {
+        basic.showString("Both Numbers are positive")
+    } else if (var1 > 0 || var2 > 0) {
+        basic.showString("At least one numbers is positive")
+    } else {
+        basic.showString("None of the numbers is positive")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (!(false)) {
+        basic.showIcon(IconNames.Yes)
+    }
+})
+let var2 = 0
+let var1 = 0
+var1 = -1
+var2 = 4
+basic.forever(function () {
+	
+})
